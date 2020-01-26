88e3a75cc39899d42a1e52c0382ac8b9bbc606da
VAIO\Siddharth <sidpatel21512@gmail.com>
2020-01-26 21:38:55 +0530

    Add and improve code-coverage support

--- src/app/github-user-details/services/github.service.spec.ts
+++ src/app/github-user-details/services/github.service.spec.ts
@@ -1,12 +1,69 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GithubService } from './github.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { of, throwError } from 'rxjs';
+import { Constants } from '../common/constants/constants';
 
 describe('GithubService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: GithubService;
+  let httpClient: HttpClient;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        HttpClient,
+        HttpHandler
+      ]
+    });
+    service = TestBed.get(GithubService);
+    httpClient = TestBed.get(HttpClient);
+  });
 
   it('should be created', () => {
-    const service: GithubService = TestBed.get(GithubService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getUserDetails', () => {
+    it('should called get api and return user profile information', () => {
+      httpClient.get = jasmine.createSpy().and.returnValue(of(''));
+      service.getUserDetails('userName').subscribe((userDetails: any) => {
+        expect(userDetails).toEqual(userDetails, 'Problem with get api not returning userDetails');
+      }, () => {
+        fail('subscribe promise failed');
+      });
+      expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should called get api and throw unhandled exception', () => {
+      httpClient.get = jasmine.createSpy().and.returnValue(throwError(''));
+      service.getUserDetails('userDetails').subscribe(() => {
+        fail('subscribe promise passed');
+      }, ((error: string) => {
+        expect(error).toEqual(Constants.genericToasterErrorMessage, 'Problem with error message');
+      }));
+      expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getUserRepos', () => {
+    it('should called get api and return user repos information', () => {
+      httpClient.get = jasmine.createSpy().and.returnValue(of(''));
+      service.getUserRepos('url').subscribe((userRepos: any) => {
+        expect(userRepos).toEqual(userRepos, 'Problem with get api not returning userRepos');
+      }, () => {
+        fail('subscribe promise failed');
+      });
+      expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should called get api and throw unhandled exception', () => {
+      httpClient.get = jasmine.createSpy().and.returnValue(throwError(''));
+      service.getUserRepos('url').subscribe(() => {
+        fail('subscribe promise passed');
+      }, ((error: string) => {
+        expect(error).toEqual(Constants.genericToasterErrorMessage, 'Problem with error message');
+      }));
+      expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
   });
 });
